1fb76bcd9936fe77b8fc9fa4cac6ef1fc88b44a9
Jamshid <j6054230@gmail.com>
2026-05-12 19:22:12 +0500

    fix: workflow strukturasi va async test xatosi to'g'irlandi

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,14 @@
-function add(a, b) { return a + b; }
+function add(a, b) {
+    return a + b;
+}
 
-// Har 10 soniyada natijani chiqarib turadi, shunda konteyner o'chmaydi
-setInterval(() => {
-  console.log("Ishlamoqda... Natija: 2 + 3 = " + add(2, 3));
-}, 10000);
-
+// Funksiyani eksport qilamiz
 module.exports = { add };
+
+// BU QISM MUHIM: Faqat fayl to'g'ridan-to'g'ri yurgizilganda ishlaydi, 
+// test vaqtida esa taymer ishga tushmaydi.
+if (require.main === module) {
+    setInterval(() => {
+        console.log("Ishlamoqda... Natija: 2 + 3 = " + add(2, 3));
+    }, 10000);
+}

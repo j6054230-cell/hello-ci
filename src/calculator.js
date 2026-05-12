7a3fe61e67074dd0739c348d9662e2e51f92a20b
Jamshid <j6054230@gmail.com>
2026-05-12 21:33:38 +0500

    fix: yaml syntax error and file separation

--- src/calculator.js
+++ src/calculator.js
@@ -2,13 +2,10 @@ function add(a, b) {
     return a + b;
 }
 
-// Funksiyani eksport qilamiz
 module.exports = { add };
 
-// BU QISM MUHIM: Faqat fayl to'g'ridan-to'g'ri yurgizilganda ishlaydi, 
-// test vaqtida esa taymer ishga tushmaydi.
 if (require.main === module) {
     setInterval(() => {
-        console.log("Ishlamoqda... Natija: 2 + 3 = " + add(2, 3));
+        console.log("Dastur ishlayapti...");
     }, 10000);
 }

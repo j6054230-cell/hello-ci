6011a33be089d6fd297a49efded052b9e87d5b99
Jamshid <j6054230@gmail.com>
2026-05-13 19:51:11 +0500

    feat: calculator tests improved and coverage fixed

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,7 +1,24 @@
-// calculator.js dan funksiyani chaqirib olamiz
-const { add } = require('../src/calculator'); 
+const { add, subtract, multiply, divide } = require('../src/calculator');
 
-test('add(2, 3) = 5 qaytarishi kerak', () => {
+describe('Calculator Tests', () => {
+  test('qo\'shishni tekshirish', () => {
     expect(add(2, 3)).toBe(5);
-});
+  });
 
+  test('ayirishni tekshirish', () => {
+    expect(subtract(10, 5)).toBe(5);
+  });
+
+  test('ko\'paytirishni tekshirish', () => {
+    expect(multiply(4, 2)).toBe(8);
+  });
+
+  test('bo\'lishni tekshirish', () => {
+    expect(divide(10, 2)).toBe(5);
+  });
+
+  // BRANCH COVERAGE UCHUN MUHIM:
+  test('nolga bo\'lish xatolik berishi kerak', () => {
+    expect(() => divide(10, 0)).toThrow("Nolga bo'lish mumkin emas");
+  });
+});
